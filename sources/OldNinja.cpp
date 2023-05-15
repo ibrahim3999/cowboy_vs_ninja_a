b3f2c1187bb56b2d27856939ac6e1c5fe4b05077
ibrahim3999 <mgdlhb@gmail.com>
2023-05-15 13:04:33 +0300

    Ninja &old ninjaclass

---
+++ sources/OldNinja.cpp
@@ -0,0 +1,11 @@
+#include"OldNinja.hpp"
+
+namespace ariel{
+    OldNinja::OldNinja(string name ,const Point& location):Ninja(name,location){
+
+    }
+    OldNinja::~OldNinja(){
+        
+    }
+
+}
